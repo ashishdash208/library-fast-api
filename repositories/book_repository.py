5b47e6a69cc6a2a5033e9b5613470918251d7cc7
swati <tiwariswati93@gmail.com>
2025-08-06 20:43:57 +0530

    feat: added new feature

--- repositories/book_repository.py
+++ repositories/book_repository.py
@@ -11,3 +11,17 @@ def create_book(book_data: BookCreate) -> Book:
     book_db.append(new_book)
     id_counter += 1
     return new_book
+
+def delete_book(book_id: int) -> bool:
+    for i, book in enumerate(book_db):
+        if book.id == book_id:
+            del book_db[i]
+            return True
+    return False
+
+def update_availability(book_id: int, available: bool) -> Optional[Book]:
+    book = get_book_by_id(book_id)
+    if book:
+        book.available = available
+        return book
+    return None
